a5980718fecc6cc6b1f21e1361fa20fd87379c9e
bsc-predict <contact@bscpredict.com>
2021-07-19 15:37:08 -0400

    tailwind css jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: 'class',
   theme: {
     colors: {
